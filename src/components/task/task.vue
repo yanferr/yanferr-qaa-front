a5416cc8b8f9e23663df9ad4bf1fbde8a9adea40
yanferrcode <859247755@qq.com>
2023-07-22 16:12:06 +0800

    任务新增内容上移快捷键/离焦自动添加序号

--- src/components/task/task.vue
+++ src/components/task/task.vue
@@ -3,8 +3,8 @@
     <el-col :xs="0" :sm="0" :md="1" :lg="4" :xl="4"></el-col>
 
     <el-col :xs="24" :sm="24" :md="22" :lg="16" :xl="16">
-      <el-input @input="handleChange" v-model="data.task" :rows="50" type="textarea" placeholder="Please input"
-        style="margin-top: 20px;" />
+      <el-input @input="handleChange" @keydown.shift.alt="handleLineMove" ref="text" v-model="data.task" :rows="50"
+        id="editor_area" type="textarea" placeholder="Please input" @blur="blurEvent" style="margin-top: 20px;" />
 
     </el-col>
 
@@ -13,9 +13,9 @@
   </el-row>
 </template>
 
-  
 <script lang="ts" setup>
-import { ref, unref, reactive, onUnmounted, onMounted } from 'vue'
+
+import { ref, defineComponent, unref, computed, reactive, onUnmounted, onMounted } from 'vue'
 import { service, bus } from "../../utils"
 import { More, Delete, InfoFilled } from '@element-plus/icons-vue'
 import { componentSizeMap, ElMessage } from 'element-plus'
@@ -26,6 +26,30 @@ let data = reactive({
   changeTime: new Date(),
   hasChanged: false,
 })
+
+// 失去焦点重新计算序号
+function blurEvent(){
+  console.log("start")
+  var lines = data.task.split('\n');
+  for(var i=0;i<lines.length;i++){
+    if(lines[i].length===0 || lines[i].trim() === ""){
+      continue;
+    }
+    if(lines[i].startsWith(">>>>>>>")){
+      break;
+    }
+    var preStr = lines[i].substring(0,lines[i].indexOf("."));
+    console.log(preStr);
+    if(!/^\d+$/.test(preStr)){
+      lines[i] = i+1+"."+lines[i];
+    }else if(parseInt(preStr)!==(i+1)){
+      lines[i] = i+1+lines[i].substring(lines[i].indexOf("."));
+    }
+  }
+  
+  data.task = lines.join('\n');
+}
+
 function handleChange() {
   data.hasChanged = true;
   data.changeTime = new Date();
@@ -33,8 +57,8 @@ function handleChange() {
 
 // 定时任务:每60s检测输入框是否发生了变化
 const intervalId = setInterval(() => {
-  
-  if (data.hasChanged ) {
+
+  if (data.hasChanged) {
     // 执行自动保存
     console.log("内容发生变化,执行自动保存...");
     autoSave();
@@ -56,13 +80,32 @@ function loadRecentTasks() {
       }
     })
 }
+function handleLineMove(event: KeyboardEvent) {
+  if (event.key === 'ArrowDown') {
+    moveLine(1, event);
+  } else if (event.key === 'ArrowUp') {
+    moveLine(-1, event);
+  }
+};
+
+
+
+function moveLine(direction, event) {
+  const lines = data.task.split('\n');
+  const caret = data.task.substring(0, event.target.selectionStart).split('\n').length - 1;
+  if (direction === -1 && caret > 0) {
+    [lines[caret - 1], lines[caret]] = [lines[caret], lines[caret - 1]];
+  } else if (direction === 1 && caret < lines.length - 1) {
+    [lines[caret], lines[caret + 1]] = [lines[caret + 1], lines[caret]];
+  }
+  data.task = lines.join('\n');
+}
 
 function autoSave() {
   service.post('qa/task/save', {
     taskId: data.taskId,
     task: data.task
   }).then(res => { // 必须要用箭头函数
-    console.log(res);
     if (res && res.data.code === 0) {
       data.taskId = res.data.taskId;
     } else {
@@ -71,11 +114,18 @@ function autoSave() {
   }).catch(function (error) {
     ElMessage.error('自动保存失败！请检查网络连接')
   });
+ 
 }
 
+computed(() => {
+
+
+})
+
 onMounted(() => {
-    // bus.emit('a', a)
-    loadRecentTasks();
+  // bus.emit('a', a)
+  loadRecentTasks();
+
 })
 
 
